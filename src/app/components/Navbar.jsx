c8f267b7d08ca1481648507febc5daa211156615
GabLunaDev <gabriellunacv@gmail.com>
2023-10-29 19:04:24 -0300

    Updating NavBar and changing font

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -33,8 +33,8 @@ const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
-            <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-5'>
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-80'>
+            <div className='flex flex-wrap items-center justify-between mx-auto px-10 py-5 backdrop-blur-md'>
                 <Link href={"/"} className='text-3xl text-white font-semibold'>
                     GabLunaDev
                 </Link>
